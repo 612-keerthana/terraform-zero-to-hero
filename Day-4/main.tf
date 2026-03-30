d028afa407961f7433c9fded7e98b3603e312104
Keerthana <49432867+612-keerthana@users.noreply.github.com>
2026-03-30 22:48:36 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,17 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "ec2_instance" {
+  ami = "ami-0f3caa1cf4417e51b"
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  key_name = "aws_devops"
+}
+
+output "ec2_public_ip_address" {
+  value = aws_instance.ec2_instance.public_ip
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "xyz-tf-project-store" # change this
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
+#No longer required
+''' resource "aws_dynamodb_table" "terraform_lock" {
   name           = "terraform-lock"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
@@ -21,4 +26,4 @@ resource "aws_dynamodb_table" "terraform_lock" {
     name = "LockID"
     type = "S"
   }
-}
+} '''

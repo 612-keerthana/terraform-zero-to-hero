06fcd8889726a30a5c5deccfbdf1a79256abcc3d
Keerthana <49432867+612-keerthana@users.noreply.github.com>
2026-03-30 22:51:04 +0530

    Update main.tf

--- Day-6/modules/ec2_instance/main.tf
+++ Day-6/modules/ec2_instance/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 variable "ami" {
-  description = "This is AMI for the instance"
+  description = "AMI image id for ec2 instance"
 }
 
 variable "instance_type" {
-  description = "This is the instance type, for example: t2.micro"
+  description = "Type of instance flavor, eg: t2.micro"
 }
 
 resource "aws_instance" "example" {
-    ami = var.ami
-    instance_type = var.instance_type
+  ami = var.ami
+  instance_type = var.instance_type
 }
